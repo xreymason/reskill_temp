1a712d56279ce8cb2adfb9b6fe5be9d9d6abc055
xreymason <dupontmason@gmail.com>
2022-08-22 16:03:44 -0400

    tests now includes testing for None

--- test_somestuff.py
+++ test_somestuff.py
@@ -2,5 +2,8 @@
 
 from somestuff import myfunc
 
-def test_myfunc():
+def test_myfunc_int_is_something():
     assert myfunc(13) == 'something'
+
+def test_myfunc_none_is_none():
+    assert myfunc(None) == None
