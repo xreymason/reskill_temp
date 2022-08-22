e3cc8f01d3951b46dab6e80cf4b67b8fd800c41b
xreymason <dupontmason@gmail.com>
2022-08-22 16:14:15 -0400

    adjusted imports for test_somestuff before moving the file to tests folder

--- test_somestuff.py
+++ test_somestuff.py
@@ -1,5 +1,7 @@
-
-
+import sys, os
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(parent_dir)
+# sys.path.append('.')
 from somestuff import myfunc
 
 def test_myfunc_int_is_something():
