6c8fff992f2d03d9ce01884c230fb173083a5dd9
xreymason <dupontmason@gmail.com>
2022-08-23 11:41:08 -0400

    Made find3 work with tuples, but not with dicts or sets. Also made argument more specific

--- tdd_practice.py
+++ tdd_practice.py
@@ -3,29 +3,30 @@
 from typing import Iterable
 
 
-def find3(iterable:Iterable) -> tuple:
+def find3(indexable:Iterable) -> tuple:
     """Gets first index for each run of 3 of 
     the same object/values in an iterable,
     runs that wrap around will use a negative 
     index.
 
-    :param iterable: object with possible sets of 3
-    :type iterable: Iterable
+    :param indexable: indexable object with possible sets of 3
+    :type indexable: Iterable
     :return: starting index for each set of 3
     :rtype: tuple
     """
-    if type(iterable) not in [list, str, tuple, set, dict]:
-        raise TypeError("passed value must be iterable")
+    acceptable_types = (list, str, tuple)
+    if type(indexable) not in acceptable_types:
+        raise TypeError("passed value must be of type " + str(acceptable_types))
     indicies = []
-    for i,v in enumerate(iterable):
-        if type(iterable) == str:
-            iterable = list(iterable)
+    for i,v in enumerate(indexable):
+        if type(indexable) in (str, tuple):
+            indexable = list(indexable)
 
-        if len(iterable) < (i+3):
-            wrap_amount=(i+3) % len(iterable)
-            wrapped_vals = iterable[i:] + iterable[:wrap_amount]
+        if len(indexable) < (i+3):
+            wrap_amount=(i+3) % len(indexable)
+            wrapped_vals = indexable[i:] + indexable[:wrap_amount]
             if ([v]*3) == wrapped_vals:
-                indicies.append(i-len(iterable))
-        elif ([v]*3) == iterable[i:i+3]:
+                indicies.append(i-len(indexable))
+        elif ([v]*3) == indexable[i:i+3]:
             indicies.append(i)
     return tuple(indicies)
