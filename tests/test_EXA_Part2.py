236568e13ee4290b50b1fe6a7ba6063803ef71e6
xreymason <dupontmason@gmail.com>
2022-08-25 15:57:20 -0400

    Fixed bugs with previous test, created new test that passes a test as a command.

--- tests/test_EXA_Part2.py
+++ tests/test_EXA_Part2.py
@@ -1,22 +1,32 @@
-from ..EXA.EXA_Part2 import ExaBot2
+from ..EXA.EXA_Part2 import ExaBot2, ExaCommand
 import pytest
+
 
 def test_passed_gt_as_comparitor():
     bot = ExaBot2()
-    assert bot._test(4, '>', 2) == True
+    bot._test(4, '>', 2)
+    assert bot.T == True
 
 
 def test_passed_lt_as_comparitor():
     bot = ExaBot2()
-    assert bot._test(5, '<', 3) == False
+    bot._test(5, '<', 3)
+    assert bot.T == False
 
 
 def test_passed_eq_as_comparitor():
     bot = ExaBot2()
-    assert bot._test(10, '=', 10) == True
+    bot._test(10, '=', 10)
+    assert bot.T == True
 
 
 def test_passed_invalid_comparitor():
     bot = ExaBot2()
     with pytest.raises(ValueError):
         bot._test(4, '+', 2)
+
+
+def test_passed_a_test():
+    bot = ExaBot2()
+    bot.process_command(ExaCommand("TEST 4 < 5"), debug=True)
+    assert bot.T == 1
