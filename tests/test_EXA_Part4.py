920600620f0c26435e081725417f1b39995e61d4
xreymason <dupontmason@gmail.com>
2022-08-26 19:25:30 -0400

    Added the final challenge as a test, still need to get this to work

--- tests/test_EXA_Part4.py
+++ tests/test_EXA_Part4.py
@@ -1,4 +1,4 @@
-from ..EXA import ExaBot4, ExaCommand
+from ..EXA import ExaBot4, ExaCommand, EXA_Commander
 import pytest
 
 
@@ -6,3 +6,11 @@ def test_passed_a_test():
     bot = ExaBot4()
     bot.process_command(ExaCommand("TEST 4 < 5"), debug=True)
     assert bot.T == 1
+
+
+def test_passed_final_challenge():
+    from ..EXA import file_system
+    print("\nBot V4 Program5")
+    bot = ExaBot4()
+    EXA_Commander.execute_file(r"C:\Users\mdupont\Downloads\reskill_temp\EXA\program5.exa", bot, debug=True)
+    assert file_system.files[400] == [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]#Might not include 1
