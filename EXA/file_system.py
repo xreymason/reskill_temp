60ff8c72d3e3fb89537d025e63bafd05649db1ff
xreymason <dupontmason@gmail.com>
2022-08-26 19:21:27 -0400

    __major__ Added file system functionality for modifying 'files' Added program5 for ExaBot4's use

--- EXA/file_system.py
+++ EXA/file_system.py
@@ -21,54 +21,73 @@ parent_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(parent_dir)
 from EXA import ExaBot
 
+
 bot_manager = {}
 files = {
-    '100':[-100, 5, 8, -3, 113],
-    '200':[],
-    '400':[]
+    100:[-100, 5, 8, -3, 113],
+    200:[],
+    400:[]
     }
 
 
 # File Modifiers
-def _file_exists(file_name:str):
-    return file_name in files
+def _file_exists(file_id:int) -> bool:
+    return file_id in files
 
 
-def _get_file(file_name:str):
-    if _file_exists(file_name):
-        return files[file_name]
+def _get_file(file_id:int) -> list:
+    if _file_exists(file_id):
+        return files[file_id]
     else:
-        raise FileNotFoundError()
+        raise FileNotFoundError("File(%r) doesn't exist in files"%(file_id))
 
 
-def create_file(file_name:str):
-    if _file_exists(str(file_name)):
-        raise FileExistsError()
-    files.update({str(file_name):[]})
-
-
-def read_file_line(file_name:str, line_number:int):
-    file = _get_file(file_name)
+def _valid_line_number(file_id:int, line_number:int) -> bool:
+    file = _get_file(file_id)
     if line_number < len(file):
-        return file[line_number]
+        return line_number
+    elif line_number == len(file):
+        return line_number
     else:
         raise ValueError("Invalid line number(%s)"%(line_number))
 
 
-def write_file_line(file_name:str, line_number:int, new_value:int):
-    file = _get_file(file_name)
-    if line_number < len(file):
+def get_file_length(file_id:int) -> int:
+    return len(_get_file(file_id))
+
+
+def create_file(file_id:int):
+    if _file_exists(str(file_id)):
+        raise FileExistsError()
+    files.update({str(file_id):[]})
+
+
+def read_file_line(file_id:int, line_number:int) -> int:
+    file = _get_file(file_id)
+    line_number = _valid_line_number(file_id, line_number)
+    return file[line_number]
+
+
+def write_file_line(file_id:int, line_number:int, new_value:int):
+    file = _get_file(file_id)
+    line_number = _valid_line_number(file_id, line_number)
+    if line_number < get_file_length(file_id):
         file[line_number] = new_value
     else:
-        raise ValueError("Invalid line number(%s)"%(line_number))
+        file.append(new_value)    
+
+
+def delete_file_line(file_id:int, line_number:int):
+    line_number = _valid_line_number(file_id, line_number)
+    files[file_id].pop(line_number)
 
 
 # Bot Usage Managers
-def _is_file_open(file_name:str):
-    return file_name in bot_manager.values()
+def _is_file_open(file_id:int) -> bool:
+    return file_id in bot_manager.values()
 
 
-def _is_bot_using_file(bot:ExaBot):
+def _is_bot_using_file(bot:ExaBot) -> bool:
     return bot in bot_manager.keys()
 
 
@@ -76,11 +95,13 @@ def bot_close_file(bot:ExaBot):
     bot_manager.pop(bot, None)
 
 
-def bot_open_file(file_name:str, bot:ExaBot):
+def bot_open_file(file_id:int, bot:ExaBot):
     if _is_bot_using_file(bot):
         raise ValueError("Bot has a File open")
-    if _is_file_open(file_name):
+    if _is_file_open(file_id):
         raise ValueError("File is open elsewhere")
-    bot_manager.update({bot:file_name})
+    bot_manager.update({bot:file_id})
 
 
+def what_file_is_bot_using(bot:ExaBot) -> int:
+    return bot_manager.get(bot, None)
